d188fb79fb3584d525ef25492e08180836ae59bb
unknown <ajayreddy911@gmail.com>
2020-05-11 14:35:50 +1000

    session2

--- solution4/src/main/java/com/example/REST/repository/user_manager_repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.REST.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.REST.model.user_manager;
-
-public interface user_manager_repository extends JpaRepository<user_manager, Integer> {
-
-}
